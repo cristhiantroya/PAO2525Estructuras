67d43335c5340cdcf23ea212a5c7dfb2c1e8b480
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-06-08 16:51:34 -0500

    Tarea Semana 2

--- EstructurasAppPAO2025/Program.cs
+++ EstructurasAppPAO2025/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Circulo
+{
+    private double radio;
+
+    public Circulo(double radio)
+    {
+        this.radio = radio;
+    }
+
+    public double CalcularArea()
+    {
+        return Math.PI * radio * radio;
+    }
+
+    public double CalcularPerimetro()
+    {
+        return 2 * Math.PI * radio;
+    }
+}
+
+public class Cuadrado
+{
+    private double lado;
+
+    public Cuadrado(double lado)
+    {
+        this.lado = lado;
+    }
+
+    public double CalcularArea()
+    {
+        return lado * lado;
+    }
+
+    public double CalcularPerimetro()
+    {
+        return 4 * lado;
+    }
+}
+
+// Clase principal con el método Main
+public class Programa
+{
+    public static void Main()
+    {
+        // Crear un círculo con radio 5
+        Circulo circulo = new Circulo(5);
+        Console.WriteLine("Área del círculo: " + circulo.CalcularArea());
+        Console.WriteLine("Perímetro del círculo: " + circulo.CalcularPerimetro());
+
+        // Crear un cuadrado con lado 4
+        Cuadrado cuadrado = new Cuadrado(4);
+        Console.WriteLine("Área del cuadrado: " + cuadrado.CalcularArea());
+        Console.WriteLine("Perímetro del cuadrado: " + cuadrado.CalcularPerimetro());
+      
+    }
+}
