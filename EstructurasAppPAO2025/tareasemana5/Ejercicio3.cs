b96e01827ddf1bfd2bf07c905dda144594298cc2
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-06-29 16:55:59 -0500

    lista asignatura

---
+++ EstructurasAppPAO2025/tareasemana5/Ejercicio3.cs
@@ -0,0 +1,13 @@
+//Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) 
+//en una lista y la muestre por pantalla.
+class Curso
+{
+    private List<string> asignaturas = new List<string>()
+    {
+        "Matemáticas",
+        "Física",
+        "Química",
+        "Historia",
+        "Lengua"
+    };
+}
