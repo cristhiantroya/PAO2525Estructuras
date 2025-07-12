630c212485cc5a388232705118c7c70834f4b4e9
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-12 17:23:38 -0500

    método para verificar balance de símbolos con pila

---
+++ EstructurasAppPAO2025/Tarea Semana 6/Tarea Semana 7/Ejercicio1.cs
@@ -0,0 +1,20 @@
+class Ejercicio1
+{
+    public static bool VerificarBalance(string expresion) // Método principal que verifica el balance de símbolos de apertura y cierre.
+    {
+        Stack<char> pila = new Stack<char>(); //Pila para controlar símbolos de apertura.
+         foreach (char caracter in expresion)
+          {
+             if (esApertura(caracter)) // Recorre cada carácter de la expresión.
+               {
+                pila.Push(caracter);
+                }
+                else if (esCierre(caracter)) 
+                 {
+                if (pila.Count == 0 || !Coinciden(pila.Pop(), caracter))
+                return false;
+                }
+        }
+        return pila.Count == 0;
+        }
+        }
