8a6089d701049844694c45a061c122094229fca1
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-12 17:29:38 -0500

    métodos para identificar y comparar símbolos

--- EstructurasAppPAO2025/Tarea Semana 6/Tarea Semana 7/Ejercicio1.cs
+++ EstructurasAppPAO2025/Tarea Semana 6/Tarea Semana 7/Ejercicio1.cs
@@ -3,18 +3,28 @@ class Ejercicio1
     public static bool VerificarBalance(string expresion) // Método principal que verifica el balance de símbolos de apertura y cierre.
     {
         Stack<char> pila = new Stack<char>(); //Pila para controlar símbolos de apertura.
-         foreach (char caracter in expresion)
-          {
-             if (esApertura(caracter)) // Recorre cada carácter de la expresión.
-               {
+        foreach (char caracter in expresion)
+        {
+            if (esApertura(caracter)) // Recorre cada carácter de la expresión.
+            {
                 pila.Push(caracter);
-                }
-                else if (esCierre(caracter)) 
-                 {
+            }
+            else if (esCierre(caracter))
+            {
                 if (pila.Count == 0 || !Coinciden(pila.Pop(), caracter))
-                return false;
-                }
+                    return false;
+            }
         }
         return pila.Count == 0;
-        }
+    }
+    // Métodos auxiliares para verificar símbolos
+    private static bool esApertura(char c) => c == '{' || c == '[' || c == '(';
+    private static bool esCierre(char c) => c == '}' || c == ']' || c == ')';
+     private static bool Coinciden(char apertura, char cierre)
+    {
+        return (apertura == '{' && cierre == '}') ||
+               (apertura == '[' && cierre == ']') ||
+               (apertura == '(' && cierre == ')');
+    }
+
         }
