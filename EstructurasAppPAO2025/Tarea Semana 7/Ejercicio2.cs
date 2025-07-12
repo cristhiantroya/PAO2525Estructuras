b669fef75fc570c031109ac773daf33284c0962c
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-12 17:53:12 -0500

    Inicializa torre A con discos y ejecuta función recursiva para moverlos

--- EstructurasAppPAO2025/Tarea Semana 7/Ejercicio2.cs
+++ EstructurasAppPAO2025/Tarea Semana 7/Ejercicio2.cs
@@ -2,8 +2,19 @@ class Ejercicio2
 {
     public static void ResolverHanoi(int discos)
     {
+        // Crear las 3 torres (pilas)
         Stack<int> torreA = new Stack<int>();
         Stack<int> torreB = new Stack<int>();
         Stack<int> torreC = new Stack<int>();
+        for (int i = discos; i >= 1; i--)
+        {
+            torreA.Push(i);
         }
+
+        Console.WriteLine("=== Estado Inicial ===");
+        MostrarTorres(torreA, torreB, torreC);
+
+        // Llamar a la función recursiva
+        MoverDiscos(discos, torreA, torreC, torreB, "A", "C", "B");
+    }
         }
