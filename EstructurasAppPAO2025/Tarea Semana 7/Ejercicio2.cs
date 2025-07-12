ebe914cbcffdda242d2f8b09100d4a3e8c9f461e
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-12 17:58:08 -0500

    Algoritmo recursivo para Torres de Hanoi

--- EstructurasAppPAO2025/Tarea Semana 7/Ejercicio2.cs
+++ EstructurasAppPAO2025/Tarea Semana 7/Ejercicio2.cs
@@ -6,6 +6,7 @@ class Ejercicio2
         Stack<int> torreA = new Stack<int>();
         Stack<int> torreB = new Stack<int>();
         Stack<int> torreC = new Stack<int>();
+
         for (int i = discos; i >= 1; i--)
         {
             torreA.Push(i);
@@ -16,5 +17,17 @@ class Ejercicio2
 
         // Llamar a la función recursiva
         MoverDiscos(discos, torreA, torreC, torreB, "A", "C", "B");
+    }
+    private static void MoverDiscos(int n, Stack<int> origen, Stack<int> destino, Stack<int> auxiliar, string nombreOrigen, string nombreDestino, string nombreAuxiliar)
+    {
+        if (n > 0)
+        {
+            MoverDiscos(n - 1, origen, auxiliar, destino, nombreOrigen, nombreAuxiliar, nombreDestino);
+            int disco = origen.Pop();
+            destino.Push(disco);
+            Console.WriteLine($"\nMover disco {disco} de {nombreOrigen} a {nombreDestino}");
+            MostrarTorres(origen, auxiliar, destino);
+            MoverDiscos(n - 1, auxiliar, destino, origen, nombreAuxiliar, nombreDestino, nombreOrigen);
+        }
     }
         }
