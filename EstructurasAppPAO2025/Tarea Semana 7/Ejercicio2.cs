57b55d40bb39a4499e8b279c64e5af170c4e346f
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-12 17:50:27 -0500

    Crear pilas para representar las torres en el problema de las Torres de Hanoi

---
+++ EstructurasAppPAO2025/Tarea Semana 7/Ejercicio2.cs
@@ -0,0 +1,9 @@
+class Ejercicio2
+{
+    public static void ResolverHanoi(int discos)
+    {
+        Stack<int> torreA = new Stack<int>();
+        Stack<int> torreB = new Stack<int>();
+        Stack<int> torreC = new Stack<int>();
+        }
+        }
