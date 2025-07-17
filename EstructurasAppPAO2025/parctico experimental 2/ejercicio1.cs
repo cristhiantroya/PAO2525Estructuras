9f9561cb0078e14a8a731691259c69222c9ab885
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-17 17:29:13 -0500

    Inicio del trabajo

---
+++ EstructurasAppPAO2025/parctico experimental 2/ejercicio1.cs
@@ -0,0 +1,6 @@
+public class Persona
+{
+    public string Nombre { get; set; } //Propiedad para registrar el nombre de la persona
+    public int NumeroTurno { get; set; } // // Propiedad para almacenar el número de turno/orden de llegada
+     
+     }
