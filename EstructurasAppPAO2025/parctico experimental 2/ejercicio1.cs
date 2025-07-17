44359deb9514315aa912dda41dddd5f41e01820e
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-17 17:30:42 -0500

    constructor

--- EstructurasAppPAO2025/parctico experimental 2/ejercicio1.cs
+++ EstructurasAppPAO2025/parctico experimental 2/ejercicio1.cs
@@ -2,5 +2,10 @@ public class Persona
 {
     public string Nombre { get; set; } //Propiedad para registrar el nombre de la persona
     public int NumeroTurno { get; set; } // // Propiedad para almacenar el número de turno/orden de llegada
+    public Persona(string nombre, int turno) // Constructor para inicializar una persona con nombre y turno
+    {
+        Nombre = nombre;
+        NumeroTurno = turno;
+    }
      
      }
