91bad599f1de37d83a3d748675fa73b5a039d6a5
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-07-17 17:42:02 -0500

    Método para mostrar los asientos ya asignados

--- EstructurasAppPAO2025/parctico experimental 2/ejercicio1.cs
+++ EstructurasAppPAO2025/parctico experimental 2/ejercicio1.cs
@@ -14,12 +14,20 @@ public class SistemaAtraccion
     private Queue<Persona> colaEspera = new Queue<Persona>(); // Cola para manejar el orden de llegada 
     private List<Persona> asientosAsignados = new List<Persona>(); // Lista para registrar los asientos asignados
     private const int CAPACIDAD = 30;
-      public void MostrarCola()  
+    public void MostrarCola()
     {
         Console.WriteLine("\n--- PERSONAS EN COLA ---");
         foreach (var persona in colaEspera) // Recorrer cada persona en la cola
         {
             Console.WriteLine($"Turno {persona.NumeroTurno}: {persona.Nombre}");
+        }
+    }
+    public void MostrarAsientos()
+    {
+        Console.WriteLine("\n--- ASIENTOS ASIGNADOS ---");
+        for (int i = 0; i < asientosAsignados.Count; i++) // Recorremos la lista de asientos asignados
+        {
+             Console.WriteLine($"Asiento {i+1}: {asientosAsignados[i].Nombre} (Turno {asientosAsignados[i].NumeroTurno})");
         }
     }
     }
