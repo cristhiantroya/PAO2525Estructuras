b6c5359ede6796e6ce1bdbf57bb73bee5fc000e1
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-08-24 12:32:50 -0500

    Correcion final

--- EstructurasAppPAO2025/Semana10.cs
+++ EstructurasAppPAO2025/Semana10.cs
@@ -2,7 +2,7 @@ public class Vacunas
 {
     public static void run()
     {
-    // Crear un conjunto de 500 ciudadanos
+    // Conjunto de 500 ciudadanos
         HashSet<string> todosCiudadanos = new HashSet<string>();
         for (int i = 1; i <= 500; i++)
         {
@@ -10,22 +10,22 @@ public class Vacunas
         }
 
         Random random = new Random();
-         // Crear conjunto de 75 vacunados con Pfizer
+    //Conjunto de 75 vacunados con Pfizer
         HashSet<string> pfizer = new HashSet<string>();
         while (pfizer.Count < 75)
 {
-    int id = random.Next(1, 501);
-    pfizer.Add("Ciudadano " + id);
+        int id = random.Next(1, 501);
+        pfizer.Add("Ciudadano " + id);
 }
-// Crear conjunto de 75 vacunados con AstraZeneca
+    //Conjunto de 75 vacunados con AstraZeneca
         HashSet<string> astrazeneca = new HashSet<string>();
         while (astrazeneca.Count < 75)
         {
             int id = random.Next(1, 501);
             astrazeneca.Add("Ciudadano " + id);
         }
-// Ciudadanos que no se han vacunado
- HashSet<string> noVacunados = new HashSet<string>();
+    // Ciudadanos que no se han vacunado
+        HashSet<string> noVacunados = new HashSet<string>();
         noVacunados.UnionWith(todosCiudadanos);
         noVacunados.ExceptWith(pfizer);
         noVacunados.ExceptWith(astrazeneca);
@@ -37,7 +37,7 @@ public class Vacunas
         }
         Console.WriteLine();
         
-        // Ciudadanos que han recibido ambas dosis
+    // Ciudadanos que han recibido ambas dosis
         HashSet<string> ambasDosis = new HashSet<string>();
         ambasDosis.UnionWith(pfizer);
         ambasDosis.IntersectWith(astrazeneca);
@@ -49,7 +49,7 @@ public class Vacunas
         }
         Console.WriteLine();
 
-        // Ciudadanos que solo han recibido Pfizer
+    // Ciudadanos que solo han recibido Pfizer
         HashSet<string> soloPfizer = new HashSet<string>();
         soloPfizer.UnionWith(pfizer);
         soloPfizer.ExceptWith(astrazeneca);
@@ -61,7 +61,7 @@ public class Vacunas
         }
         Console.WriteLine();
 
-        // Ciudadanos que solo han recibido AstraZeneca
+    // Ciudadanos que solo han recibido AstraZeneca
         HashSet<string> soloAstrazeneca = new HashSet<string>();
         soloAstrazeneca.UnionWith(astrazeneca);
         soloAstrazeneca.ExceptWith(pfizer);
