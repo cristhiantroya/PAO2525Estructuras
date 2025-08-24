1869abb971541d8bab4cf95f32e422f66742fbf0
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-08-24 11:52:02 -0500

    conjunto de 75 vacunados con AstraZeneca

--- EstructurasAppPAO2025/Semana10.cs
+++ EstructurasAppPAO2025/Semana10.cs
@@ -12,8 +12,14 @@ public class vacunas
          // Crear conjunto de 75 vacunados con Pfizer
         HashSet<string> pfizer = new HashSet<string>();
         while (pfizer.Count < 75)
+{
+    int id = random.Next(1, 501);
+    pfizer.Add("Ciudadano " + id);
+}
+// Crear conjunto de 75 vacunados con AstraZeneca
+        HashSet<string> astrazeneca = new HashSet<string>();
+        while (astrazeneca.Count < 75)
         {
             int id = random.Next(1, 501);
-            pfizer.Add("Ciudadano " + id);
+            astrazeneca.Add("Ciudadano " + id);
         }
-}
