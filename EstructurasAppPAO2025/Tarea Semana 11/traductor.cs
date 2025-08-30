4fa076a32ea3ccbcbc9cface5d7e77b00584b90e
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-08-30 18:05:26 -0500

    agregar palabras iniciales

--- EstructurasAppPAO2025/Tarea Semana 11/traductor.cs
+++ EstructurasAppPAO2025/Tarea Semana 11/traductor.cs
@@ -4,6 +4,28 @@ public class traductorbasico
     {
         //Creamos el dicccionario 
         Dictionary<string, string> diccionario = new Dictionary<string, string>();
+        //Agregamos las palabras iniciales 
+        diccionario.Add("time", "tiempo");
+        diccionario.Add("person", "persona");
+        diccionario.Add("year", "año");
+        diccionario.Add("way", "camino");
+        diccionario.Add("day", "día");
+        diccionario.Add("thing", "cosa");
+        diccionario.Add("man", "hombre");
+        diccionario.Add("world", "mundo");
+        diccionario.Add("life", "vida");
+        diccionario.Add("hand", "mano");
+        diccionario.Add("part", "parte");
+        diccionario.Add("child", "niño");
+        diccionario.Add("eye", "ojo");
+        diccionario.Add("woman", "mujer");
+        diccionario.Add("place", "lugar");
+        diccionario.Add("work", "trabajo");
+        diccionario.Add("week", "semana");
+        diccionario.Add("case", "caso");
+        diccionario.Add("point", "punto");
+        diccionario.Add("government", "gobierno");
+        diccionario.Add("company", "empresa");
    
     }
 
