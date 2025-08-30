96519717a4b16d0b374ba26924a6f38f0f6b1143
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-08-30 18:02:23 -0500

    Crear diccionario

---
+++ EstructurasAppPAO2025/Tarea Semana 11/traductor.cs
@@ -0,0 +1,10 @@
+public class traductorbasico
+{
+    public static void run()
+    {
+        //Creamos el dicccionario 
+        Dictionary<string, string> diccionario = new Dictionary<string, string>();
+   
+    }
+
+}
