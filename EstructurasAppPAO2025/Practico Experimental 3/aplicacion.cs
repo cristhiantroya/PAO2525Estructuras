0d1f14b5ce19e2bf76461038c97db258084bcb21
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-09-06 16:35:08 -0500

    Diccionario para alamcenar los libros

--- EstructurasAppPAO2025/Practico Experimental 3/aplicacion.cs
+++ EstructurasAppPAO2025/Practico Experimental 3/aplicacion.cs
@@ -2,8 +2,8 @@ public class registrolibros
 {
     public static void run()
     {
-
-        
+        //Diccionario para almacener los libros 
+         Dictionary<string, string> libros = new Dictionary<string, string>();
    
     }
 
