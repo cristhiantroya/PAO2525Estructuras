b78fc9178095174e898aa4df8fecef1f4cdf6f4a
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-09-06 16:38:18 -0500

    Conjunto para las categorias de los libros

--- EstructurasAppPAO2025/Practico Experimental 3/aplicacion.cs
+++ EstructurasAppPAO2025/Practico Experimental 3/aplicacion.cs
@@ -3,8 +3,11 @@ public class registrolibros
     public static void run()
     {
         //Diccionario para almacener los libros 
-         Dictionary<string, string> libros = new Dictionary<string, string>();
-   
+        Dictionary<string, string> libros = new Dictionary<string, string>();
+
+        // Conjunto para las categorias de los libros 
+        HashSet<string> categorias = new HashSet<string>();
+    
     }
 
 }
