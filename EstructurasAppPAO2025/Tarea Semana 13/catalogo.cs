a6e47eb925bea7ec5bb2ea302da12cbc11091906
cristhiantroya <cf.troyaj@uea.edu.ec>
2025-09-13 17:54:56 -0500

    añadir método Insertar para agregar títulos en el arreglo de revistas

--- EstructurasAppPAO2025/Tarea Semana 13/catalogo.cs
+++ EstructurasAppPAO2025/Tarea Semana 13/catalogo.cs
@@ -6,7 +6,16 @@ public class catalogoderevistas
         {
              revistas = new string[20];
               cantidad = 0;
-              
+              }
+    //añadir método Insertar para agregar títulos en el arreglo de revistas
+        public void Insertar(string titulo)
+    {
+        if (cantidad < revistas.Length)
+        {
+            revistas[cantidad] = titulo;
+            cantidad++;
+        }
+    }  
     public static void run()
     {
 
